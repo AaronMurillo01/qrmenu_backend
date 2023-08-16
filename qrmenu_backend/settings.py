0a08433a865108bedd46870294b772fde00b5ede
Aaron Murillo <amurillo11@cnm.edu>
2023-08-16 17:27:47 -0600

    Moved os import to the top in settings.py

--- qrmenu_backend/settings.py
+++ qrmenu_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,4 +145,3 @@ except:
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-import os
